fe3f4c8ad44e951ffb0c8ffd34bb04dfd3e91a8f
Jakob Schwarz <jakob.schwarz97@gmail.com>
2023-12-07 16:17:47 +0100

    Readme update

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
-from werkzeug.utils import secure_filename
+#from werkzeug.utils import secure_filename
 import subprocess
 import json
 import sys
