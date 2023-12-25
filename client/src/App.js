b31977497f681d6692b4b57d980457aae94c73c3
Jakob Schwarz <jakob.schwarz97@gmail.com>
2023-12-25 18:45:07 +0100

    Removed old js files not used  anymore or never used before

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FileUpload from './FileUpload';
 import CharacterData from './CharacterData'
 import StartScreen from './StartScreen';
 import CharacterManagement from './CharacterManagement';
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<StartScreen />} />
-        <Route path="/upload" element={<FileUpload />} />
         <Route path="/talents/:characterName" element={<CharacterData />} />
         <Route path="/characters" element={<CharacterManagement />} />
       </Routes>
